8fd985dfeddc04d83061779ffbe027de3308bf32
tango3 <lakshay.8026@gmail.com>
2025-06-18 16:58:42 +0000

    Im

--- src/helpers/axios_helper.js
+++ src/helpers/axios_helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8081';
+axios.defaults.baseURL = 'http://springboot-backend:8081';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const userUrl = "/api/v1/users";
